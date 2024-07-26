f202f206b79f7b57bb1b69db0fdfbc573a779e41
VyeoooT <vlinh52000@gmail.com>
2024-07-26 15:08:59 +0700

    - Delete imgContact

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,11 +1,9 @@
-import imgContact from './contactImg.png'
 import logo from './logo.png'
 
 // img project
 import photogram from './projects/study-photogram.png'
 
 export default {
-    imgContact,
     logo,
 
     photogram,
