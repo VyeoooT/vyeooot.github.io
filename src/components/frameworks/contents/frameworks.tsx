d58c5671de28e68ddeac6e7cb72655053f3096f8
VyeoooT <vlinh52000@gmail.com>
2024-07-26 15:10:30 +0700

    - Update frameworks

--- src/components/frameworks/contents/frameworks.tsx
+++ src/components/frameworks/contents/frameworks.tsx
@@ -13,7 +13,6 @@ function Frameworks() {
                 <div className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-14">
                     <Grid imgSrc="https://contents.rizerssoft.com/uploads/React_icon_3fbc10a062.svg" label="React"/>
                     <Grid imgSrc="https://contents.rizerssoft.com/uploads/Tailwind_CSS_Logo_f2f2137d87.svg" label="Tailwind CSS"/>
-                    <Grid imgSrc="https://contents.rizerssoft.com/uploads/thumbnail_Vue_1628d24640.png" label="Vue"/>
                 </div>
             </div>
         </>
